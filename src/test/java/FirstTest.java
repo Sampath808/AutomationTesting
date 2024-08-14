9dddfb97a6fb58d513cfa9c973db24a29bf81130
Sampath808 <106588302+Sampath808@users.noreply.github.com>
2024-08-15 02:03:02 +0530

    FirstTest.java

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,15 +1,24 @@
 
+import atu.testrecorder.ATUTestRecorder;
+import atu.testrecorder.exceptions.ATUTestRecorderException;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -17,6 +26,23 @@ import java.util.List;
 public class FirstTest  {
 
     WebDriver driver;
+    private ATUTestRecorder recorder;
+
+    @BeforeMethod
+    public synchronized void beforeMethod(Method method) throws MalformedURLException {
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("ddMMyyHHmmss");
+        try {
+            recorder = new ATUTestRecorder(System.getProperty("user.dir")+"\\Videos\\",method.getName()+"_"+simpleDateFormat.format(date),false);
+        } catch (ATUTestRecorderException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            recorder.start();
+        } catch (ATUTestRecorderException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public void setUpBrowser(String browser) {
         switch (browser.toLowerCase()) {
@@ -41,7 +67,7 @@ public class FirstTest  {
 
     @Test(description = "screen Resolution Test",dataProvider = "screenResolution" )
     public void screenResolutionTest(int dimension1,int dimension2) throws InterruptedException {
-        setUpBrowser("safari");
+        setUpBrowser("chrome");
         driver.get("https://www.getcalley.com/page-sitemap.xml");
         driver.manage().window().setSize(new Dimension(dimension1,dimension2));
         List<WebElement> links = driver.findElements(By.xpath("(//*[@id='sitemap']//a)[position()<6]"));
@@ -53,7 +79,7 @@ public class FirstTest  {
                 driver.navigate().back();
                 Thread.sleep(1000);
             }catch (StaleElementReferenceException staleElementReferenceException){
-                links = driver.findElements(By.xpath("(//*[@id='sitemap']//a)[position()<6]"));
+                links = driver.findElements(By.xpath("(//*[@id='sitemap']//a)[position()<3]"));
                 links.get(i).click();
                 takeScreenshot();
                 Thread.sleep(2000);
@@ -94,6 +120,15 @@ public class FirstTest  {
 
         return data;
 
+    }
+
+    @AfterMethod
+    public void afterMethod(ITestResult result){
+        try {
+            recorder.stop();
+        } catch (ATUTestRecorderException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
